7f058734e13bebc83c1d9ef33f66e16f0c3f2b39
Noahwus <noahwus@gmail.com>
2017-12-27 14:41:31 -0600

    Dicking with Jumping

--- Assets/Player/PlayerPlatformerController.cs
+++ Assets/Player/PlayerPlatformerController.cs
@@ -4,27 +4,45 @@ using UnityEngine;
 
 public class PlayerPlatformerController : PhysicsObject {
 
-	public float jumpTakeOffSpeed = 7;
+	public float jumpTakeOffSpeed = 14;
+	public float accel = 2;
 	public float maxSpeed = 10;
+	public float fallSpeed = .8f;
 
+	//private float deltaY = 0f;
 
-	void Start () {
-		
+	Vector2 move = Vector2.zero;
+
+	private SpriteRenderer spriteRenderer;
+	private Animator animator;
+
+	void Awake () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		animator = GetComponent<Animator> ();
+		//gravMod = gravModifier;
 	}
 	
 	protected override void ComputeVelocity(){
-		Vector2 move = Vector2.zero;
 
 		move.x = Input.GetAxis ("Horizontal");
 
-		if (Input.GetButtonDown ("Jump") && grounded) {
+		if (!grounded && velocity.y <= 0) {
+			velocity.y = velocity.y * fallSpeed;
+		} else if (Input.GetButtonDown ("Jump") && grounded) {
 			velocity.y = jumpTakeOffSpeed;
 		} else if (Input.GetButtonUp ("Jump")) {
 			if (velocity.y > 0) {
-				velocity.y = velocity.y - 1;
+				velocity.y = velocity.y * -fallSpeed;
 			}
+		} 
+
+		bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+		if(flipSprite){
+			spriteRenderer.flipX = !spriteRenderer.flipX;
 		}
 
+		//animator.SetBool ("grounded", grounded);
+		//animator.SetFloat ("VelocityX", Mathf.Abs (velocity.x) / maxSpeed);
 
 		targetVelocity = move * maxSpeed;
 
