3f504a08dbc83869f8763e9916d6b5ade6fa6f1a
Kayla Techmeier <k.mae.tech@gmail.com>
2017-12-27 11:46:08 -0600

    lists pass!
    wip on the list checker but lists pass and there are no errors

---
+++ Assets/Scripts/UseItemToPopulateFields.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+public class UseItemToPopulateFields : MonoBehaviour {
+
+	public int itemNumber;
+	public Text title;
+	public Text flavorText;
+	public Text cost;
+	public Image icon;
+	public Image popupIcon;
+	//private ItemsArray itemArray;
+
+	void Start(){
+		//itemArray = FindObjectOfType<ItemsArray> ();
+
+		//PopulateInfo();
+	}
+
+
+	public void PopulateInfo()
+	{
+		print (itemNumber);
+		//title.text =  itemArray.itemsArray [itemNumber].title;
+		//flavorText.text = itemArray.itemsArray [itemNumber].flavorText;
+		//icon.sprite = itemArray.itemsArray [itemNumber].icon;
+		//popupIcon.sprite = itemArray.itemsArray [itemNumber].icon;
+
+
+
+	}
+}
